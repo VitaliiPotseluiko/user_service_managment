bb48e53c253cf66e6842e022b236485fc2859094
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-06-26 17:59:26 +0300

    renamed main class

--- src/main/java/com/vitalii/userservicemanagment/UserServiceManagementApplication.java
+++ src/main/java/com/vitalii/userservicemanagment/UserServiceManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceManagmentApplication {
+public class UserServiceManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceManagmentApplication.class, args);
+        SpringApplication.run(UserServiceManagementApplication.class, args);
     }
 
 }
