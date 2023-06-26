5ef1ac9b98226f197aa15c8a67955306f4624336
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-06-26 17:52:24 +0300

    first commit

---
+++ src/main/java/com/vitalii/userservicemanagment/UserServiceManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.vitalii.userservicemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vitalii/userservicemanagment/UserServiceManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalii.userservicemanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
